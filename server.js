1967de31105d5c310b9b8772dc5ce17759d3c7c8
paralisacaoprogen-collab <paralisacaoprogen@gmail.com>
2026-03-26 18:57:13 -0300

    Create server.js with Node.js server code for real-time synchronization using Socket.IO

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Socket.IO Server</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
